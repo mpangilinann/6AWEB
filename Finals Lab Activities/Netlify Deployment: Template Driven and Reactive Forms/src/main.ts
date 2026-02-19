24e2d3d653427b0a44d48189b531307696378a78
mpangilinann <mikkopangilinan39@gmail.com>
2026-02-19 09:06:56 +0800

    Delete Finals Lab Activities/Netlify Deployment: Template Driven and Reactive Forms directory

--- Finals Lab Activities/Netlify Deployment: Template Driven and Reactive Forms/src/main.ts
+++
@@ -1,7 +0,0 @@
-import '@angular/compiler'; // Add this at the very top
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app'; // Change 'App' to 'AppComponent'
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
